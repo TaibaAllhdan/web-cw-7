d5444710c69dfcfbc98595078a57fda6c51f9098
TaibaAllhdan <76259135+TaibaAllhdan@users.noreply.github.com>
2021-01-05 21:06:21 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,19 @@
 // قم بحل السؤال الأول هنا
+
+let pets=["cat","dog","rabbit","parrot","turtle" ];
+   
+
+pets.pop(pets[4])
+console.log(pets)
+
+
+let A= prompt (' what your favorite pats?');
+
+
+let dog={
+    name: "kiwi",
+    age:'3 years old',
+    emoji:"🐶"
+}
+console.log(dog.emoji);
+
